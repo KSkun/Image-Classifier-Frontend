34448f4e5e7c2baf100fbbbc37cb92036310fe9d
KSkun <ks@ksmeow.moe>
2022-01-03 22:42:43 +0800

    chore: remove unused logic

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,7 @@ export const appName = '快速文本/非文本图像分类系统'
 export const urlPrefix = 'http://localhost:5000'
 
 export default {
-  name: 'App',
-  mounted() {
-    if (localStorage.tokenExpire < Date.now()) {
-      localStorage.removeItem('token')
-      localStorage.removeItem('tokenExpire')
-      localStorage.login = false
-    }
-  }
+  name: 'App'
 }
 </script>
 
